3c6e160372a0997908e442b48cc0dbd7aeca8ced
Kristy Heijenk <ksheijenk@hotmail.com>
2026-03-04 10:02:17 +0100

    Added exercise 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,11 +12,11 @@ public class Main {
     static void main(String[] args) {
         LocalDate today = LocalDate.now();
 
-        // Exercise 1 - Current Date
+// Exercise 1 - Current Date
         System.out.println("Today: " + today);
 
 
-        // Exercise 2 - Formatted Current Date
+// Exercise 2 - Formatted Current Date
         /*
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("eeee dd MMMM", new Locale("sv", "SE"));
         String formattedTime = today.format(formatter);
@@ -25,7 +25,7 @@ public class Main {
 
          */
 
-        // Exercise 3 - Last Monday and the Entire Week
+// Exercise 3 - Last Monday and the Entire Week
         LocalDate lastMonday = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
 
         System.out.println("Last Monday: "+ lastMonday);
@@ -37,34 +37,35 @@ public class Main {
             LocalDate day = lastMonday.plusDays(i);
             System.out.println(day.format(formatter));
         }
+// Exercise 4 - Parse Date from String
 
+        LocalDate parseDate = LocalDate.parse("2026-03-04");
 
+        System.out.println("Parse LocalDate: " + parseDate);
 
-        // Exercise 4 - Parse Date from String
+// Exercise 5 - Day of the Week
 
-        // Exercise 5 - Day of the Week
+// Exercise 6 - Future and Past Date
 
-        // Exercise 6 - Future and Past Date
+// Exercise 7 - Period Between Dates
 
-        // Exercise 7 - Period Between Dates
+// Exercise 8 - Add Period to Current Date
 
-        // Exercise 8 - Add Period to Current Date
+// Exercise 9 - Current Time
 
-        // Exercise 9 - Current Time
+// Exercise 10 - Nanoseconds of LocalTime
 
-        // Exercise 10 - Nanoseconds of LocalTime
+// Exercise 11 - Parse Time from String
 
-        // Exercise 11 - Parse Time from String
+// Exercise 12 - Formatted Current Time
 
-        // Exercise 12 - Formatted Current Time
+// Exercise 13 - Specific LocalDateTime
 
-        // Exercise 13 - Specific LocalDateTime
+// Exercise 14 - Formatted LocalDateTime
 
-        // Exercise 14 - Formatted LocalDateTime
+// Exercise 15 - Combine Date and Time
 
-        // Exercise 15 - Combine Date and Time
-
-        // Exercise 16 - Extract Components from LocalDateTime
+// Exercise 16 - Extract Components from LocalDateTime
 
 
 
